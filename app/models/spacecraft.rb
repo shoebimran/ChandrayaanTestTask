5604afb97571a533272ce473d39ff1df77732d92
Shoeb Imran <shoebimran005@gmail.com>
2024-01-25 17:09:06 +0530

    added a move_backword method

--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -7,4 +7,25 @@ class Spacecraft < ApplicationRecord
     self.x += 1 if direction == 'E'
     self.x -= 1 if direction == 'W'
   end
+
+  def move_backward
+    case direction
+    when 'N' then move_along_axis(:y, -1)
+    when 'S' then move_along_axis(:y, 1, max_bound: 9_999_999)
+    when 'E' then move_along_axis(:x, -1)
+    when 'W' then move_along_axis(:x, 1)
+    end
+  end
+
+  private
+
+  def move_along_axis(axis, distance, max_bound: nil)
+    current_position = send(axis)
+    new_position = current_position - distance
+
+    unless max_bound && !(-max_bound..max_bound).cover?(new_position)
+      send("#{axis}=", new_position)
+    end
+  end
+  
 end
--- spec/models/spacecraft_spec.rb
+++ spec/models/spacecraft_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Spacecraft, type: :model do
       expect(spacecraft.y).to eq(1)
     end
   end
+
+  describe '#move_backward' do
+    it 'moves the spacecraft one step backward in the current direction' do
+      spacecraft = Spacecraft.new(x: 0, y: 0, z: 0, direction: 'N')
+      spacecraft.move_backward
+      expect(spacecraft.y).to eq(1) # The spacecraft moves in the negative y direction
+    end
+
+    it 'does not move beyond the specified max bound in the positive y direction' do
+      spacecraft = Spacecraft.new(x: 0, y: 9_999_999, z: 0, direction: 'S')
+      spacecraft.move_backward
+      expect(spacecraft.y).to eq(9_999_998) # Should not move beyond the boundary
+    end
+
+    it 'moves backward in the negative x direction' do
+      spacecraft = Spacecraft.new(x: 1, y: 0, z: 0, direction: 'W')
+      spacecraft.move_backward
+      expect(spacecraft.x).to eq(0)
+    end
+  end
 end
